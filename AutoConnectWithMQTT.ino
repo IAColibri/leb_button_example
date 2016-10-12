86685d52ab07973516c67b39f024e15f60091b6f
olvap <oldani.pablo@gmail.com>
2016-10-12 17:48:39 -0300

    Fix: extract custom params setup to it own file.

--- AutoConnectWithMQTT.ino
+++ AutoConnectWithMQTT.ino
@@ -17,25 +17,11 @@
 
 #include "pin.h"
 #include "device.h"
+#include "config.h"
 // 1 led, 1 button
 #define LED 0
 #define BUTTON 2
 #define DEBUG_MODE  1               //1 set debug on
-
-//define your default values here, if there are different values in config.json, they are overwritten.
-char mqtt_server[40];
-char mqtt_port[6] = "1883";
-char device_id[6] = "1";
-char device_name[40] = "esp8266";
-
-//flag for saving data
-bool shouldSaveConfig = false;
-
-//callback notifying us of the need to save config
-void saveConfigCallback () {
-  Serial.println("Should save config");
-  shouldSaveConfig = true;
-}
 
 BasicButton button = BasicButton(BUTTON);
 Pin led(LED);
@@ -72,118 +58,7 @@ void setup() {
   Serial.begin(115200);
   Serial.println();
 
-  //read configuration from FS json
-  Serial.println("mounting FS...");
-  delay(2000);
-  if (SPIFFS.begin()) {
-    Serial.println("mounted file system");
-    if (SPIFFS.exists("/config.json")) {
-      //file exists, reading and loading
-      Serial.println("reading config file");
-      File configFile = SPIFFS.open("/config.json", "r");
-      if (configFile) {
-        Serial.println("opened config file");
-        size_t size = configFile.size();
-        // Allocate a buffer to store contents of the file.
-        std::unique_ptr<char[]> buf(new char[size]);
-
-        configFile.readBytes(buf.get(), size);
-        DynamicJsonBuffer jsonBuffer;
-        JsonObject& json = jsonBuffer.parseObject(buf.get());
-	Serial.println("config.json");
-        json.printTo(Serial);
-        if (json.success()) {
-          Serial.println("\nparsed json");
-
-          strcpy(mqtt_server, json["mqtt_server"]);
-          strcpy(mqtt_port, json["mqtt_port"]);
-          strcpy(device_id, json["device_id"]);
-          strcpy(device_name, json["device_name"]);
-
-        } else {
-          Serial.println("failed to load json config");
-        }
-      }
-    }
-  } else {
-    Serial.println("failed to mount FS");
-  }
-  //end read
-
-  // The extra parameters to be configured (can be either global or just in the setup)
-  // After connecting, parameter.getValue() will get you the configured value
-  // id/name placeholder/prompt default length
-  WiFiManagerParameter custom_mqtt_server("server", "mqtt server", mqtt_server, 40);
-  WiFiManagerParameter custom_mqtt_port("port", "mqtt port", mqtt_port, 6);
-  WiFiManagerParameter custom_device_id("device_id", "device id", device_id, 6);
-  WiFiManagerParameter custom_device_name("device_name", "device name", device_name, 40);
-
-  //WiFiManager
-  //Local intialization. Once its business is done, there is no need to keep it around
-  //WiFiManager
-  //Local intialization. Once its business is done, there is no need to keep it around
-  WiFiManager wifiManager;
-
-  //set config save notify callback
-  wifiManager.setSaveConfigCallback(saveConfigCallback);
-
-  //add all your parameters here
-  wifiManager.addParameter(&custom_mqtt_server);
-  wifiManager.addParameter(&custom_mqtt_port);
-  wifiManager.addParameter(&custom_device_id);
-  wifiManager.addParameter(&custom_device_name);
-
-  //reset settings - for testing
-  //wifiManager.resetSettings();
-
-  //tries to connect to last known settings
-  //if it does not connect it starts an access point with the specified name
-  //here  "AutoConnectAP" with password "password"
-  //and goes into a blocking loop awaiting configuration
-  if (!wifiManager.autoConnect("AutoConnectAP", "password")) {
-    Serial.println("failed to connect, we should reset as see if it connects");
-    delay(3000);
-    ESP.reset();
-    delay(5000);
-  }
-
-  //if you get here you have connected to the WiFi
-  Serial.println("connected...yeey :)");
-
-  //read updated parameters
-  strcpy(mqtt_server, custom_mqtt_server.getValue());
-  strcpy(mqtt_port, custom_mqtt_port.getValue());
-  strcpy(device_id, custom_device_id.getValue());
-  strcpy(device_name, custom_device_name.getValue());
-
-  //save the custom parameters to FS
-  if (shouldSaveConfig) {
-    Serial.println("saving config");
-    DynamicJsonBuffer jsonBuffer;
-    JsonObject& json = jsonBuffer.createObject();
-    json["mqtt_server"] = mqtt_server;
-    json["mqtt_port"] = mqtt_port;
-    json["device_id"] = device_id;
-    json["device_name"] = device_name;
-
-    File configFile = SPIFFS.open("/config.json", "w");
-    if (!configFile) {
-      Serial.println("failed to open config file for writing");
-    }
-
-    json.printTo(Serial);
-    json.printTo(configFile);
-    configFile.close();
-    //end save
-  }
-  Serial.println("local ip");
-  Serial.println(WiFi.localIP());
-
-  // --------------
-  Serial.print("mqtt_server: ");
-  Serial.println(mqtt_server);
-  Serial.print("port: ");
-  Serial.println(atoi(mqtt_port));
+  setup_config();
   client.setServer(mqtt_server, atoi(mqtt_port));
   client.setCallback(callback);
   // --------------
